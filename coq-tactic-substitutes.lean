d655c465c4a85c935a51b0fa058c5e95ff3764ee
Ben <bmsherman@users.noreply.github.com>
2017-09-25 15:18:16 -0700

    Remove our specialize tactic
    (Now that specialize is in the standard library)

--- coq-tactic-substitutes.lean
+++ coq-tactic-substitutes.lean
@@ -7,22 +7,6 @@ local postfix *:9001 := many
 
 namespace tactic.interactive
 
-private
-meta def specialize_get_name : expr → tactic name
-| (app f _) := specialize_get_name f
-| (local_const _ n _ _) := pure n
-| _ := fail "Not an application of a local constant"
-
-/-- The analogue of Coq's `specialize` tactic -/
-meta def specialize (H : parse texpr) : tactic unit :=
-  do result ← i_to_expr H,
-     id ← specialize_get_name result,
-     n ← get_unused_name id none,
-     note n none result,
-     to_remove ← get_local id, 
-     tactic.clear to_remove,
-     tactic.rename n id
-
 universes u v
 
 def congr_arg_f_equal {A : Sort u} {B : Sort v} {f f' : A → B}
